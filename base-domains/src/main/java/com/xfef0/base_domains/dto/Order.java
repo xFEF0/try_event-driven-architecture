6fb7f28ecda4e21bc4b92915655205dc5cc5e1d6
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-03-26 13:16:10 -0300

    add DTO order classes

---
+++ base-domains/src/main/java/com/xfef0/base_domains/dto/Order.java
@@ -0,0 +1,15 @@
+package com.xfef0.base_domains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    private String orderId;
+    private String name;
+    private int quantity;
+    private double price;
+}
